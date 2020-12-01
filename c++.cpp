5a63d40b712c414a2c45cb071a63dbab15d588e4
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-01 21:21:37 +0700

    Create c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "enter first numble" << endl;
+    cin >> a;
+    cout << "enter first numble" << endl;
+    cin >> b;
+    cout << "enter first numble" << endl;
+    cin >> c;
+    cout << a << endl << b << endl << c << endl;
+    cout << c << endl << b << endl << a << endl;
+    return(0);
+}
+
